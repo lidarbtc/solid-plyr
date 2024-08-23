26533b5deab0a4ed1cdfa508eed43fe74e674c71
lidar <97009593+lidarbtc@users.noreply.github.com>
2024-08-23 20:56:15 +0900

    Fix: npm i

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,26 @@
 module.exports = {
-  "root": true,
-  "extends": [
-    "lxsmnsyc/typescript"
-  ],
-  "parserOptions": {
-    "project": "./tsconfig.eslint.json"
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+    ecmaVersion: 2021,
+    sourceType: "module",
   },
-  "rules": {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  rules: {
     "import/no-extraneous-dependencies": [
       "error",
       {
-        "devDependencies": [
-          "**/*.test.tsx",
-          "vite.config.ts"
-        ]
-      }
+        devDependencies: ["**/*.test.tsx", "vite.config.ts"],
+      },
     ],
-    "camelcase": "off",
+    camelcase: "off",
     "react/button-has-type": "off",
     "react/destructuring-assignment": "off",
     "react/jsx-props-no-spreading": "off",
@@ -26,6 +30,6 @@ module.exports = {
     "react/prop-types": "off",
     "jsx-a11y/anchor-is-valid": "off",
     "react/no-unknown-property": "off",
-    "jsx-a11y/label-has-associated-control": "off"
-  }
+    "jsx-a11y/label-has-associated-control": "off",
+  },
 };
